e5ec365186dcafff39e6a484eb1161de6b539d89
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-14 21:10:40 +0500

    Application Abstruction implemeted

---
+++ Chat.Application/Abstruction/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Chat.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chat.Application.Abstruction
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Domain.Entities.Chat> Chats { get; set; }
+
+        public ValueTask<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
