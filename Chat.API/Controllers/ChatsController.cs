88125b2ff909a23e5bdcf771180205c62f4c9ded
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-14 19:56:20 +0500

    Controllers implemeted

--- Chat.API/Controllers/ChatsController.cs
+++ Chat.API/Controllers/ChatsController.cs
@@ -1,28 +1,27 @@
-﻿using Chat.API.Entities;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Chat.API.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
-    public class AccountsController : ControllerBase
+    public class ChatsController : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> GetAccountsAsync()
+        public async Task<IActionResult> GetChatsAsync()
         {
 
-            return Ok(new List<Account>()
+            return Ok(new List<Entities.Chat>()
             {
-                new Account()
+                new Entities.Chat()
                 {
                     Id = 1,
-                    Name = "Account1",
+                    Name = "Chat1",
                     CreatedAt = DateTime.Now,
                 },
-                new Account()
+                new Entities.Chat()
                 {
                     Id = 2,
-                    Name = "Account2",
+                    Name = "Chat2",
                     CreatedAt = DateTime.Now,
                 },
             });
---
+++ Chat.API/Controllers/MessagesController.cs
@@ -0,0 +1,37 @@
+﻿using Chat.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetMessagesAsync()
+        {
+
+            return Ok(new List<Message>()
+            {
+                new Message()
+                {
+                    Id = 1,
+                    Text = "Message1",
+                    CreatedAt = DateTime.Now
+                },
+                new Message()
+                {
+                    Id = 2,
+                    Text = "Message2",
+                    CreatedAt = DateTime.Now
+                },
+                new Message()
+                {
+                    Id = 3,
+                    Text = "Message3",
+                    CreatedAt = DateTime.Now
+                },
+            });
+        }
+    }
+}
