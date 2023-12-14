b5b5c3723f7622d079c8c3144ffd28807d9af7a5
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-14 19:33:22 +0500

    Chat api qo'shildi

---
+++ Chat.API/Controllers/AccountsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+    }
+}
