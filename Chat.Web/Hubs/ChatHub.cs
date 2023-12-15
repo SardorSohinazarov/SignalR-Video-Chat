f83dd9ed4bb28e0a16f7c393b7b410018f008498
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-15 18:21:47 +0500

    Shunaqa

--- Chat.Web/Hubs/ChatHub.cs
+++ Chat.Web/Hubs/ChatHub.cs
@@ -1,15 +1,71 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Runtime.CompilerServices;
+using System.Threading.Channels;
 
 namespace Chat.Web.Hubs
 {
     public class ChatHub : Hub
     {
+        public async IAsyncEnumerable<int> Counter(
+            int count,
+            int delay,
+            [EnumeratorCancellation]
+            CancellationToken cancellationToken)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                // Check the cancellation token regularly so that the server will stop
+                // producing items if the client disconnects.
+                cancellationToken.ThrowIfCancellationRequested();
+
+                yield return i;
+
+                // Use the cancellationToken in other APIs that accept cancellation
+                // tokens so the cancellation can flow down to them.
+                await Task.Delay(delay, cancellationToken);
+            }
+        }
+
+        private async Task WriteItemsAsync(
+            ChannelWriter<int> writer,
+            int count,
+            int delay,
+            CancellationToken cancellationToken)
+        {
+            Exception localException = null;
+            try
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    await writer.WriteAsync(i, cancellationToken);
+
+                    // Use the cancellationToken in other APIs that accept cancellation
+                    // tokens so the cancellation can flow down to them.
+                    await Task.Delay(delay, cancellationToken);
+                }
+            }
+            catch (Exception ex)
+            {
+                localException = ex;
+            }
+            finally
+            {
+                writer.Complete(localException);
+            }
+        }
+
         public async Task SendToAllMessage(string user, string message)
             => await Clients.All.SendAsync("ReceiveMessage", user, Context.ConnectionId , message);
         
         public async Task SendToUserMessage(string id,string user, string message)
-            => await Clients.Client(id).SendAsync("ReceiveMessageFromUser", user, message);
+        {
+            await Clients.Client(id).SendAsync("ReceiveMessageFromUser", user, message);
+            await Clients.Caller.SendAsync("ReceiveMessageFromUser", user, message);
+
+        }
         
+        
+
         public async Task SendToGroupMessage(string groupName,string user, string message)
             => await Clients.Group(groupName).SendAsync("ReceiveMessageFromGroup", user, message);
 
